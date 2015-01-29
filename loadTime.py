e96b826cfd359022c0307bb78b4d6f06c6b7f65b
mstallone <mstallone@users.noreply.github.com>
2015-01-28 23:09:29 -0800

    Flushing DNS cache before every request

--- loadTime.py
+++ loadTime.py
@@ -1,10 +1,9 @@
 import requests, datetime, sys, subprocess
 
-subprocess.call("sudo discoveryutil udnsflushcaches", shell=True)
-
 totalTime = datetime.timedelta(0,0)
 
 for i in range(int(sys.argv[2])):
+    subprocess.call("sudo discoveryutil udnsflushcaches", shell=True)
     totalTime += requests.request('GET', str(sys.argv[1])).elapsed
-    
+
 print(str(totalTime/int(sys.argv[2])))
